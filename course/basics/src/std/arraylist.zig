54279aaf0c252147a96d9176b587fc7820e80fef
zdzDesigner <1696498456@qq.com>
2024-08-12 11:34:57 +0800

    ~

--- course/basics/src/std/arraylist.zig
+++ course/basics/src/std/arraylist.zig
@@ -28,3 +28,46 @@ test "MultiArrayList:" {
     const list = std.ArrayList(Player).init(std.testing.allocator);
     std.debug.print("list:{}\n", .{list});
 }
+
+test "toOwnedSlice:" {
+    var list = std.ArrayList(Player).init(std.testing.allocator);
+    defer list.deinit();
+
+    try list.append(.{ .name = "111", .isdead = true });
+    try list.append(.{ .name = "222", .isdead = false });
+    try list.append(.{ .name = "333", .isdead = true });
+    try list.append(.{ .name = "444", .isdead = true });
+    try list.append(.{ .name = "555", .isdead = false });
+    // std.debug.print("list:{}\n", .{list});
+
+    const listitems = list.items.ptr[0..];
+    std.debug.print("items =========== \n:{any}\n", .{listitems});
+
+    const list_own = try list.toOwnedSlice();
+    std.debug.print("list_own =========== \n:{any}\n", .{list_own});
+    defer std.testing.allocator.free(list_own); // 单个释放
+
+    const listcap = list.allocatedSlice();
+    std.debug.print("listcap:{any}\n", .{listcap});
+    defer std.testing.allocator.free(listcap); // 单个释放
+}
+
+test "toOwnedSlice::arena:" {
+    var arena = std.heap.ArenaAllocator.init(std.testing.allocator);
+    defer arena.deinit(); // 统一释放, 无需单个释放
+    var list = std.ArrayList(Player).init(arena.allocator());
+
+    try list.append(.{ .name = "111", .isdead = true });
+    try list.append(.{ .name = "222", .isdead = false });
+    try list.append(.{ .name = "333", .isdead = true });
+    try list.append(.{ .name = "444", .isdead = true });
+    try list.append(.{ .name = "555", .isdead = false });
+    const listitems = list.items.ptr[0..];
+    std.debug.print("items =========== \n:{any}\n", .{listitems});
+
+    const list_own = try list.toOwnedSlice();
+    std.debug.print("list_own =========== \n:{any}\n", .{list_own});
+
+    const listcap = list.allocatedSlice();
+    std.debug.print("listcap:{any}\n", .{listcap});
+}
